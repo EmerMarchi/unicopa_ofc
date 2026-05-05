ed79637e5090510af54d79a2eb4124991a280e40
Emerson Jean <marchimeson123@gmail.com>
2026-05-05 19:14:01 -0300

    parte inicial

--- App.js
+++ App.js
@@ -1,45 +1,36 @@
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, SectionList } from 'react-native';
 import GameCard from './components/GameCard';
+import dados from './assets/dados.json';
 
 export default function App() {
 
-  const jogos = [
-    {
-      "id": 1,
-      "fase": "Fase de grupos",
-      "grupo": "A",
-      "data_et": "2026-06-11",
-      "hora_et": "15:00",
-      "data_brasilia": "2026-06-11",
-      "hora_brasilia": "16:00",
-      "time_casa": "México",
-      "sigla_casa": "MEX",
-      "time_fora": "África do Sul",
-      "sigla_fora": "RSA",
-      "confronto": "México x África do Sul",
-      "estadio": "Estádio Azteca",
-      "cidade": "Cidade do México",
-      "pais": "México"
-    },
-    {
-      "id": 2,
-      "fase": "Fase de grupos",
-      "grupo": "A",
-      "data_et": "2026-06-11",
-      "hora_et": "22:00",
-      "data_brasilia": "2026-06-11",
-      "hora_brasilia": "23:00",
-      "time_casa": "Coreia do Sul",
-      "sigla_casa": "KOR",
-      "time_fora": "Tchéquia",
-      "sigla_fora": "CZE",
-      "confronto": "Coreia do Sul x Tchéquia",
-      "estadio": "Estádio Akron",
-      "cidade": "Guadalajara",
-      "pais": "México"
-    }
-  ]
+  const jogos = dados.jogos;
 
+  const agruparPorData = (jogos) => {
+    return jogos.reduce((acc, jogo) => {
+
+      const data = jogo.data_brasilia; // data do jogo
+      if (!acc[data]) {
+        acc[data] = []; // se a data ainda não existe no acumulador, cria um array para ela
+      }
+
+      acc[data].push(jogo); // adiciona o jogo ao array da data correspondente
+
+      return acc; // retorna o acumulador para a próxima iteração
+    }, {}); // reduce: reduz um array a um único valor, acumulando os resultados em um objeto
+  };
+
+  const jogosAgrupados = agruparPorData(jogos); // agrupa os jogos por data
+
+  const jogosTratados = Object.keys(jogosAgrupados).map(data => {
+    return {
+      title: data,
+      data: jogosAgrupados[data]
+    };
+  }); // transforma o objeto de jogos agrupados em um array de objetos, onde cada objeto tem uma propriedade "title" 
+      // com a data e uma propriedade "dados" com os jogos daquela data 
+
+      console.log(jogosTratados); // exibe o array de jogos tratados no console
   return (
     <ImageBackground style={styles.container}
       source={require('./assets/bg-overlay.png')}>
@@ -51,16 +42,23 @@ export default function App() {
 
       <Text style={styles.title}>CALENDÁRIO</Text>
 
-      <View style={styles.card}>
+      <SectionList
+        sections={jogosTratados}
+        keyExtractor={(item, index) => item + index}
+        renderItem={() => null}
+        renderSectionHeader={ ({section}) => (
+          <View style={styles.card}>
+            <Text style={styles.data}>{section.title}</Text>
+            {
+              section.data.map(jogo => (
+                <GameCard key={jogo.id} game={jogo} />
+              ))
+            }
+          </View>
+        )
+      }
+      />
 
-        <Text style={styles.data}>
-          {jogos[0].data_brasilia.split('-').slice(1).reverse().join('/')}
-        </Text>
-
-        <GameCard game={jogos[0]} />
-        <GameCard game={jogos[1]} />
-
-      </View>
     </ImageBackground>
   );
 }
